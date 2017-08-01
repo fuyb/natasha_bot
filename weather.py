4ee5c2c0f2677c3f607a43a412f9aac3c822d96d
fuyanin <been1986@gmail.com>
2017-08-02 00:35:33 +0800

    天气输出格式不换行

--- weather.py
+++ weather.py
@@ -16,9 +16,20 @@ def fetch_weather(location):
         results = data.get('results')
         if results:
             now = results[0].get('now')
-            result = u'{city}\n天气：{weather}\n气温：{temperature}°C'.format(
+            result = u'{city}\t天气：{weather}\t气温：{temperature}°C'.format(
                     city=location,
                     weather=now.get('text'),
                     temperature=now.get('temperature'))
             return result
     return u'查询天气失败，或不支持这个城市'
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description=u'实时天气查询')
+    parser.add_argument('-l', '--location',
+                        dest='location',
+                        required=True,
+                        help=u'城市名')
+
+    options = parser.parse_args()
+    print fetch_weather(options.location.decode('utf-8'))
